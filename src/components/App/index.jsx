35270b8346dfd7afc92a4808144694b045a0e46b
PajaCzechita <p.bajgarova@gmail.com>
2023-05-27 17:17:09 +0200

    React Router added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Footer } from '../Footer';
-import { Header } from '../Header';
-import { Home } from '../Home';
+import React from "react";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
+import { Home } from "../Home";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Reservation } from "../Reservation";
 
-export const App = () => (
-  <>
-    <Header />
-    <Home />
-    <Footer />
-  </>
-);
+export const App = () => {
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    {
+      path: "/reservation",
+      element: <Reservation />,
+    },
+  ]);
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
+};
